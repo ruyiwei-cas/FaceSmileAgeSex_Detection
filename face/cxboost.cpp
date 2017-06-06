73e0be49143333d2fdfd54f23c36fdc68ea4e86a
yiwei ru <ruyiwei.cas@gmail.com>
2017-06-06 22:14:39 +0800

    Update cxboost.cpp

--- face/cxboost.cpp
+++ face/cxboost.cpp
@@ -4,7 +4,7 @@
 *** The information and source code contained herein is the exclusive
 *** property of Intel Corporation and may not be disclosed, examined
 *** or reproduced in whole or in part without explicit written authorization
-*** from the company.
+*** from the company.    test
 ***
 *** Embedded Application Lab, Intel Labs China.
 **/
